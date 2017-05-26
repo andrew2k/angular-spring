0c5c95f035d25a0ccfc61f7f8534f9a8ad385462
andrew2k <a.franceschini@IRC15308.replynet.prv>
2017-05-26 12:09:51 +0200

    added sql script

--- src/main/java/it/reply/dao/db_ddl_scripts.sql
+++ src/main/java/it/reply/dao/db_ddl_scripts.sql
@@ -10,15 +10,14 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 CREATE TABLE users (
+	user_id SERIAL PRIMARY KEY
     username character varying(45) NOT NULL,
     password character varying(45) NOT NULL,
     profile character varying(45),
     enabled integer DEFAULT 1 NOT NULL
 );
 ALTER TABLE users OWNER TO postgres;
-ALTER TABLE ONLY users ADD CONSTRAINT users_pkey PRIMARY KEY (username);
-
-
+CREATE UNIQUE INDEX users_username_idx ON users (username);
 
 CREATE TABLE registratino_waiting_list (
     username character varying(45) NOT NULL,
@@ -29,53 +28,22 @@ CREATE TABLE registratino_waiting_list (
 ALTER TABLE registratino_waiting_list OWNER TO postgres;
 ALTER TABLE ONLY registratino_waiting_list ADD CONSTRAINT registratino_waiting_list_pkey PRIMARY KEY (code);
 
-
-
-
 CREATE TABLE user_roles (
-    user_role_id integer NOT NULL,
+    user_role_id SERIAL PRIMARY KEY,
     username character varying(45) NOT NULL,
-    profile character varying(45) NOT NULL
+    role character varying(45) NOT NULL
 );
 ALTER TABLE user_roles OWNER TO postgres;
-
-CREATE SEQUENCE user_roles_user_role_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-ALTER TABLE user_roles_user_role_id_seq OWNER TO postgres;
-ALTER SEQUENCE user_roles_user_role_id_seq OWNED BY user_roles.user_role_id;
-ALTER TABLE ONLY user_roles ALTER COLUMN user_role_id SET DEFAULT nextval('user_roles_user_role_id_seq'::regclass);
-SELECT pg_catalog.setval('user_roles_user_role_id_seq', 3, true);
-
-ALTER TABLE ONLY user_roles ADD CONSTRAINT user_roles_pkey PRIMARY KEY (user_role_id);
 ALTER TABLE ONLY user_roles ADD CONSTRAINT user_roles_role_username_key UNIQUE (role, username);
-ALTER TABLE ONLY user_roles ADD CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES users(username);
-
-
+--ALTER TABLE ONLY user_roles ADD CONSTRAINT fk__user_roles__username FOREIGN KEY (username) REFERENCES users(username);
 
 CREATE TABLE profile_roles (
-    profile_role_id integer NOT NULL,
+    profile_role_id SERIAL PRIMARY KEY,
     role character varying(45) NOT NULL,
     profile character varying(45) NOT NULL
 );
 ALTER TABLE profile_roles OWNER TO postgres;
-
-CREATE SEQUENCE profile_roles_profile_role_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-ALTER TABLE profile_roles_profile_role_id_seq OWNER TO postgres;
-ALTER SEQUENCE profile_roles_profile_role_id_seq OWNED BY profile_roles.profile_role_id;
-ALTER TABLE ONLY profile_roles ALTER COLUMN profile_role_id SET DEFAULT nextval('profile_roles_profile_role_id_seq'::regclass);
-SELECT pg_catalog.setval('profile_roles_profile_role_id_seq', 3, true);
-
-ALTER TABLE ONLY profile_roles ADD CONSTRAINT profile_roles_pkey PRIMARY KEY (profile_role_id);
-ALTER TABLE ONLY profile_roles ADD CONSTRAINT profile_roles_role_profile_key UNIQUE (role, profile);
+ALTER TABLE ONLY profile_roles ADD CONSTRAINT profile_roles__role_profile_key UNIQUE (role, profile);
 --ALTER TABLE ONLY profile_roles ADD CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES users(username);
 
 
@@ -92,11 +60,11 @@ GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
 COPY profile_roles(profile, role) FROM stdin;
-USER	ROLE_USER2
+USER_PROFILE	ROLE_USER2
 \.
 
 COPY users (username, password, profile, enabled) FROM stdin;
-mkyong	123456	USER	1
+mkyong	123456	USER_PROFILE	1
 alex	123456		1
 \.
 
@@ -105,3 +73,17 @@ COPY user_roles (user_role_id, username, role) FROM stdin;
 2	mkyong	ROLE_ADMIN
 3	alex	ROLE_USER
 \.
+
+delete from registratino_waiting_list;
+delete from profile_roles
+delete from user_roles;
+delete from users;
+insert into profile_roles values ('USER_PROFILE', 'role1');
+
+
+drop table registratino_waiting_list;
+drop table profile_roles
+drop table user_roles;
+drop table users;
+
+
